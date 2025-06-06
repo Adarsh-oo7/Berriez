8c49b45e6f7842db36f4b228c4b3859456b5c02e
Adarsh-oo7 <adarshsarachandran@gmail.com>
2025-06-06 17:24:18 +0530

    try to fix the error

--- app/robots.ts
+++ app/robots.ts
@@ -1,5 +1,8 @@
 import type { MetadataRoute } from "next"
 
+// Add this line to mark the route as static
+export const dynamic = "force-static";
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
