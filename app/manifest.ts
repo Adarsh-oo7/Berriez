2acf28a1933ad1f8b23a47d4eb4b3a298e21e5a1
Adarsh-oo7 <adarshsarachandran@gmail.com>
2025-06-06 17:22:13 +0530

    error fixing

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,5 +1,8 @@
 import type { MetadataRoute } from "next"
 
+// Add this line to explicitly mark the route as static
+export const dynamic = "force-static";
+
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: "Squeeze Berriez - Best Natural Drinks in Thrissur",
